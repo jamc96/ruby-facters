889e064a506282749e3bdfeeb1251a3eb2f56563
Jose Mejia <jamc.mejia@gmail.com>
2018-03-05 14:47:35 -0600

    test chunks

--- bond.rb
+++ bond.rb
@@ -3,13 +3,11 @@ Facter.add(:bond, :type => :aggregate) do
   confine :kernel => "Linux"
     chunk(:status) do
       status = Facter.value(:interfaces).split(',').any? { |key| key =~ /^bond(\d+)/ } ? 'true' : 'false'
-      unless status.to_s.empty
-        bonding = {:status => status}  
-      end
+      status unless status.to_s.empty
     end
     chunk(:interface) do
       interface = Facter.value(:interfaces).split(',').find { |key| key =~ /^bond(\d+)/}
-      bonding = {:interface => interface} unless interface.to_s.empty
+      interface unless interface.to_s.empty
     end
 end
 
