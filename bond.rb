25b0c90564b482e0f21e9dffbb7dde46cd04f99c
Jose Mejia <jamc.mejia@gmail.com>
2018-03-05 14:39:32 -0600

    removing variable test

--- bond.rb
+++ bond.rb
@@ -1,7 +1,6 @@
 # Facter to determine if exist a bonding
 Facter.add(:bond, :type => :aggregate) do
   confine :kernel => "Linux"
-  bonding = {}
     chunk(:status) do
       status = Facter.value(:interfaces).split(',').any? { |key| key =~ /^bond(\d+)/ } ? 'true' : 'false'
       unless status.to_s.empty
