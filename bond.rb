6ebcbccc749b8bdcc9db8bb9788262722a4d7c2a
Jose Mejia <jamc.mejia@gmail.com>
2018-03-05 14:35:32 -0600

    fixing test

--- bond.rb
+++ bond.rb
@@ -1,16 +1,14 @@
 # Facter to determine if exist a bonding
 Facter.add(:bond, :type => :aggregate) do
   confine :kernel => "Linux"
-
+  bonding = {}
     chunk(:status) do
-      bonding = {}
       status = Facter.value(:interfaces).split(',').any? { |key| key =~ /^bond(\d+)/ } ? 'true' : 'false'
       unless status.to_s.empty
         bonding = {:status => status}  
       end
     end
     chunk(:interface) do
-      bonding = {}
       interface = Facter.value(:interfaces).split(',').find { |key| key =~ /^bond(\d+)/}
       bonding = {:interface => interface} unless interface.to_s.empty
     end
