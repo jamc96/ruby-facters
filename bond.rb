6cde781bdc160d127c87f7249acd1716377e1506
Jose Mejia <jamc.mejia@gmail.com>
2018-03-05 16:22:46 -0600

    last commit fix

--- bond.rb
+++ bond.rb
@@ -1,13 +1,13 @@
-# Facter to determine if exist a bonding
+# Facter to determine if exist a bond
 Facter.add(:bond, :type => :aggregate) do
-  confine :kernel => "Linux"
+  confine :kernel => 'Linux'
     chunk(:status) do
-      status = Facter.value(:interfaces).split(',').any? { |key| key =~ /^bond(\d+)/ } ? 'true' : 'false'
-      status unless status.to_s.empty
+      bond = { :status => Facter.value(:interfaces).split(',').any? { |key| key =~ /bond(\d+)/ } ? 'true' : 'false' }
     end
     chunk(:interface) do
-      interface = Facter.value(:interfaces).split(',').find { |key| key =~ /^bond(\d+)/}
-      interface unless interface.to_s.empty
+      if Facter.value(:interfaces) =~ /bond(\d+)/
+        bond = { :interface => Facter.value(:interfaces).split(',').find { |key| key =~ /bond(\d+)/} } 
+      end
     end
 end
 
